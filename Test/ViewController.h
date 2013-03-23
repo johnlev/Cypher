a3f2cd4211f8fd8d5a1865f8202811b74ff33e52
John Kotz <john.lyme@mac.com>
2013-03-23 09:50:31 -0400

    Support for passes, fix for key reading and clearing, a hide keyboard button, and a clear and exit button

--- Test/ViewController.h
+++ Test/ViewController.h
@@ -15,10 +15,13 @@
 @property (nonatomic,retain) IBOutlet UITextField *key;
 @property (nonatomic,retain) IBOutlet UISegmentedControl *choice;
 @property (nonatomic,retain) IBOutlet UIButton *clear;
+@property (nonatomic,retain) IBOutlet UISegmentedControl *passes;
 -(IBAction)clicked:(id)sender;
 -(IBAction)copyr:(id)sender;
 -(IBAction)clear:(id)sender;
 -(IBAction)paster:(id)sender;
 -(IBAction)showActionsheetButton:(id)sender;
+-(IBAction)hide:(id)sender;
 -(void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex;
+-(IBAction)clearAndExit:(id)sender;
 @end
--- Test/ViewController.m
+++ Test/ViewController.m
@@ -11,11 +11,11 @@
 #import <MessageUI/MessageUI.h>
 
 @interface ViewController ()
-
+-(void) terminateWithSuccess;
 @end
 
 @implementation ViewController
-@synthesize button,label,encrypt,key,choice,clear;
+@synthesize button,label,encrypt,key,choice,clear,passes;
 - (void)mailComposeController:(MFMailComposeViewController*)controller
           didFinishWithResult:(MFMailComposeResult)result
                         error:(NSError*)error;
@@ -24,6 +24,12 @@
         NSLog(@"It's away!");
     }
     [self dismissModalViewControllerAnimated:YES];
+}
+-(IBAction)clearAndExit:(id)sender{
+    encrypt.text = NULL;
+    label.text = NULL;
+    key.text = NULL;
+    exit(0);
 }
 -(IBAction)paster:(id)sender{
     encrypt.text = [UIPasteboard generalPasteboard].string;
@@ -34,160 +40,190 @@
 -(IBAction)copyr:(id)sender{
     [UIPasteboard generalPasteboard].string = label.text;
 }
--(IBAction)clicked:(id)sender{
+-(IBAction)hide:(id)sender{
     [self.view endEditing:YES];
-    if (choice.selectedSegmentIndex == 0) {
-        char *alphabet1 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .?!:;'<>1234567890,/";
-        int i = 0;
-        int s = 0;
-        int x = 0;
-        int z = 0;
-        int a = [key.text intValue];
-        int lengthAlpha = strlen(alphabet1);
-        char alphabet2[lengthAlpha];
-        int array[lengthAlpha];
-        int inOrOut = 1;
-        NSUInteger length = [encrypt.text length];
-        unichar input[length];
-        unichar output[length];
-        for (unsigned int i = 0; i < length; i++) {
-            input[i] = [encrypt.text characterAtIndex:i];
-        }
-        x=0;
-        printf("%d", a);
-        int y = 0;
-        i = 0;
-        while (x < lengthAlpha) {
-            srand(a);
-            z = rand()%(lengthAlpha - 1);
-            // Check the random place in the array for the presence of another char
-            while (i < lengthAlpha && x < lengthAlpha) {
-                if (z == array[i]) {
-                    z = (z + 1) % (lengthAlpha + 1);
-                    i=-1;
+}
+-(IBAction)clicked:(id)sender{
+    int passed = 0;
+    printf("[");
+    [self.view endEditing:YES];
+    while (passed < passes.selectedSegmentIndex + 1) {
+        if (choice.selectedSegmentIndex == 0) {
+            char *alphabet1 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .?!:;'<>1234567890,/";
+            int i = 0;
+            int s = 0;
+            int x = 0;
+            int z = 0;
+            int a = [key.text intValue];
+            int lengthAlpha = strlen(alphabet1);
+            char alphabet2[lengthAlpha];
+            int array[lengthAlpha];
+            int inOrOut = 1;
+            NSUInteger length = [encrypt.text length];
+            unichar input[length];
+            unichar output[length];
+            if (passed == 0){
+                for (unsigned int i = 0; i < length; i++) {
+                    input[i] = [encrypt.text characterAtIndex:i];
                 }
-                i++;
-                y++;
+            }else{
+                for (unsigned int i = 0; i < length; i++) {
+                    input[i] = [label.text characterAtIndex:i];
+                }
+            }
+            for (int b=0; b<[encrypt.text length];b++) {
+                if (input[b] == '\n') {
+                    input[b] = ' ';
+                }
+            }
+            x=0;
+            printf("%d", a);
+            int y = 0;
+            i = 0;
+            while (x < lengthAlpha) {
+                srand(a);
+                z = rand()%(lengthAlpha - 1);
+                // Check the random place in the array for the presence of another char
+                while (i < lengthAlpha && x < lengthAlpha) {
+                    if (z == array[i]) {
+                        z = (z + 1) % (lengthAlpha + 1);
+                        i=-1;
+                    }
+                    i++;
+                    y++;
+                }
+                i=0;
+                // Puts it in the new alphabet
+                alphabet2[z] = alphabet1[x];
+                array[x] = z;
+                x++;
             }
             i=0;
-            // Puts it in the new alphabet
-            alphabet2[z] = alphabet1[x];
-            array[x] = z;
-            x++;
-        }
-        i=0;
-        while (array[i] < lengthAlpha) {
-            array[i] = 0;
-            i++;
-        }
-        i=0;
-        //Encode it
-        while (input[i] != '\0' && inOrOut == 1) {
-            while (TRUE) {
-                if (input[i] == alphabet1[s]) {
-                    //sets the origanal letter to be the encrypted letter
-                    output[i] = alphabet2[s];
-                    break;
-                } else if (alphabet1[s] == '\0') {
-                    //if an unknown character is inputted, it will use this: _
-                    output[i] = '-';
-                    break;
-                }
-                s++;
-            }
-            i++;
-            s=0;
-        }
-        NSString* string = [NSString stringWithCharacters:output length:length];
-        label.text = string;
-    }else if (choice.selectedSegmentIndex == 1){
-        char *alphabet1 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .?!:;'<>1234567890,/";
-        int i = 0;
-        int s = 0;
-        int x = 0;
-        int z = 0;
-        int a = [key.text intValue];
-        int lengthAlpha = strlen(alphabet1);
-        char alphabet2[lengthAlpha];
-        int array[lengthAlpha];
-        int inOrOut = 2;
-        NSUInteger length = [encrypt.text length];
-        unichar input[length];
-        unichar output[length];
-        for (unsigned int i = 0; i < length; i++) {
-            input[i] = [encrypt.text characterAtIndex:i];
-        }
-        x=0;
-        printf("%d", a);
-        int y = 0;
-        i = 0;
-        while (x < lengthAlpha) {
-            srand(a);
-            z = rand()%(lengthAlpha - 1);
-            // Check the random place in the array for the presence of another char
-            while (i < lengthAlpha && x < lengthAlpha) {
-                if (z == array[i]) {
-                    z = (z + 1) % (lengthAlpha + 1);
-                    i=-1;
-                }
+            while (array[i] < lengthAlpha) {
+                array[i] = 0;
                 i++;
-                y++;
             }
             i=0;
-            // Puts it in the new alphabet
-            alphabet2[z] = alphabet1[x];
-            array[x] = z;
-            x++;
-        }
-        i=0;
-        while (array[i] < lengthAlpha) {
-            array[i] = 0;
-            i++;
-        }
-        i=0;
-        //Encode it
-        //        while (input[i] != '\0' && inOrOut == 1) {
-        //            while (TRUE) {
-        //                if (input[i] == alphabet1[s]) {
-        //                    //sets the origanal letter to be the encrypted letter
-        //                    output[i] = alphabet2[s];
-        //                    break;
-        //                } else if (alphabet1[s] == '\0') {
-        //                    //if an unknown character is inputted, it will use this: _
-        //                    output[i] = '-';
-        //                    break;
-        //                }
-        //                s++;
-        //            }
-        //            i++;
-        //            s=0;
-        //        }
-        while (input[i] != '\0' && inOrOut == 2) {
-            while (TRUE) {
-                if (input[i] == alphabet2[s]) {
-                    //sets the encrypted letter to be the origanal letter
-                    output[i] = alphabet1[s];
-                    break;
-                } else if (alphabet2[s] == '\0') {
-                    //if an unknown character is inputted, it will use this: _
-                    output[i] = '-';
-                    break;
+            //Encode it
+            while (input[i] != '\0' && inOrOut == 1) {
+                while (TRUE) {
+                    if (input[i] == alphabet1[s]) {
+                        //sets the origanal letter to be the encrypted letter
+                        output[i] = alphabet2[s];
+                        break;
+                    } else if (alphabet1[s] == '\0') {
+                        //if an unknown character is inputted, it will use this: _
+                        output[i] = '-';
+                        break;
+                    }
+                    s++;
                 }
-                s++;
+                i++;
+                s=0;
             }
-            i++;
-            s=0;
+            NSString* string = [NSString stringWithCharacters:output length:length];
+            label.text = string;
         }
-        NSString* string = [NSString stringWithCharacters:output length:length];
-        label.text = string;
+        passed++;
     }
-    
+    passed = 0;
+    while (passed < passes.selectedSegmentIndex + 1) {
+        if (choice.selectedSegmentIndex == 1){
+            char *alphabet1 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .?!:;'<>1234567890,/";
+            int i = 0;
+            int s = 0;
+            int x = 0;
+            int z = 0;
+            int a = [key.text intValue];
+            int lengthAlpha = strlen(alphabet1);
+            char alphabet2[lengthAlpha];
+            int array[lengthAlpha];
+            int inOrOut = 2;
+            NSUInteger length = [encrypt.text length];
+            unichar input[length];
+            unichar output[length];
+            if (passed == 0){
+                for (unsigned int i = 0; i < length; i++) {
+                    input[i] = [encrypt.text characterAtIndex:i];
+                }
+            }else{
+                for (unsigned int i = 0; i < length; i++) {
+                    input[i] = [label.text characterAtIndex:i];
+                }
+            }
+            x=0;
+            printf("%d", a);
+            int y = 0;
+            i = 0;
+            while (x < lengthAlpha) {
+                srand(a);
+                z = rand()%(lengthAlpha - 1);
+                // Check the random place in the array for the presence of another char
+                while (i < lengthAlpha && x < lengthAlpha) {
+                    if (z == array[i]) {
+                        z = (z + 1) % (lengthAlpha + 1);
+                        i=-1;
+                    }
+                    i++;
+                    y++;
+                }
+                i=0;
+                // Puts it in the new alphabet
+                alphabet2[z] = alphabet1[x];
+                array[x] = z;
+                x++;
+            }
+            i=0;
+            while (array[i] < lengthAlpha) {
+                array[i] = 0;
+                i++;
+            }
+            i=0;
+            //Encode it
+            //        while (input[i] != '\0' && inOrOut == 1) {
+            //            while (TRUE) {
+            //                if (input[i] == alphabet1[s]) {
+            //                    //sets the origanal letter to be the encrypted letter
+            //                    output[i] = alphabet2[s];
+            //                    break;
+            //                } else if (alphabet1[s] == '\0') {
+            //                    //if an unknown character is inputted, it will use this: _
+            //                    output[i] = '-';
+            //                    break;
+            //                }
+            //                s++;
+            //            }
+            //            i++;
+            //            s=0;
+            //        }
+            while (input[i] != '\0' && inOrOut == 2) {
+                while (TRUE) {
+                    if (input[i] == alphabet2[s]) {
+                        //sets the encrypted letter to be the origanal letter
+                        output[i] = alphabet1[s];
+                        break;
+                    } else if (alphabet2[s] == '\0') {
+                        //if an unknown character is inputted, it will use this: _
+                        output[i] = '-';
+                        break;
+                    }
+                    s++;
+                }
+                i++;
+                s=0;
+            }
+            NSString* string = [NSString stringWithCharacters:output length:length];
+            label.text = string;
+        }
+        passed++;
+    }
+    key.text = NULL;
 }
 - (IBAction)showActionsheetButton:(id)sender{
     NSArray *activityItems = @[label.text];
     NSArray *applicationItems = [[NSArray alloc] initWithObjects:/*UIActivityTypePostToFacebook, UIActivityTypePostToTwitter, UIActivityTypePostToWeibo, UIActivityTypeMessage, UIActivityTypeMail, UIActivityTypePrint, UIActivityTypeCopyToPasteboard, UIActivityTypeAssignToContact, UIActivityTypeSaveToCameraRoll, */nil];
     UIActivityViewController *activityViewController = [[UIActivityViewController alloc] initWithActivityItems:activityItems applicationActivities:applicationItems];
-    activityViewController.excludedActivityTypes = @[UIActivityTypePostToWeibo, UIActivityTypeAssignToContact];
+    activityViewController.excludedActivityTypes = @[UIActivityTypePostToWeibo, UIActivityTypeAssignToContact, UIActivityTypeCopyToPasteboard];
     [self presentViewController:activityViewController animated:YES completion:NULL];
 }
 
@@ -208,7 +244,7 @@
 //            // Handle the error
 //        }
 //    }
-//    
+//
 //    if(buttonIndex == 1)
 //    {
 //        NSArray *activityItems = @[@"Hello",[UIImage imageNamed:@"someimage"]];
@@ -227,7 +263,7 @@
 //            // Handle the error
 //        }
 //    }
-//     
+//
 //}
 
 
@@ -235,9 +271,73 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    key.text = NULL;
-    encrypt.text = NULL;
-    label.text = NULL;
+    char *alphabet1 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .?!:;'<>1234567890,/";
+    int i = 0;
+    int s = 0;
+    int x = 0;
+    int z = 0;
+    int a = 5;
+    int lengthAlpha = strlen(alphabet1);
+    char alphabet2[lengthAlpha];
+    int array[lengthAlpha];
+    int inOrOut = 1;
+    NSUInteger length = 5;
+    char hi[] = "Hello";
+    unichar input[length];
+    unichar output[length];
+    for (unsigned int i = 0; i < length; i++) {
+        input[i] = hi[i];
+    }
+    for (int b=0; b<length;b++) {
+        if (input[b] == '\n') {
+            input[b] = ' ';
+        }
+    }
+    x=0;
+    printf("%d", a);
+    int y = 0;
+    i = 0;
+    while (x < lengthAlpha) {
+        srand(a);
+        z = rand()%(lengthAlpha - 1);
+        // Check the random place in the array for the presence of another char
+        while (i < lengthAlpha && x < lengthAlpha) {
+            if (z == array[i]) {
+                z = (z + 1) % (lengthAlpha + 1);
+                i=-1;
+            }
+            i++;
+            y++;
+        }
+        i=0;
+        // Puts it in the new alphabet
+        alphabet2[z] = alphabet1[x];
+        array[x] = z;
+        x++;
+    }
+    i=0;
+    while (array[i] < lengthAlpha) {
+        array[i] = 0;
+        i++;
+    }
+    i=0;
+    //Encode it
+    while (input[i] != '\0' && inOrOut == 1) {
+        while (TRUE) {
+            if (input[i] == alphabet1[s]) {
+                //sets the origanal letter to be the encrypted letter
+                output[i] = alphabet2[s];
+                break;
+            } else if (alphabet1[s] == '\0') {
+                //if an unknown character is inputted, it will use this: _
+                output[i] = '-';
+                break;
+            }
+            s++;
+        }
+        i++;
+        s=0;
+    }
 }
 
 -(void)viewDidDisappear:(BOOL)animated{
